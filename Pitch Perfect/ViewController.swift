af52df36845b29c76510d56846a6b4aefc18d5a2
Shu Yan <yanshu.usc@gmail.com>
2014-12-29 00:03:47 -0500

    Add second screen

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +23,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        // Hide the 
+        stopButton.hidden = true
+        recordButton.enabled = true
+    }
 
     @IBAction func recordAudio(sender: UIButton) {
+        stopButton.hidden = false
         recordingInProgress.hidden = false
+        recordButton.enabled = false
         // TODO: record the voice
         println("in recordAudio")
     }
